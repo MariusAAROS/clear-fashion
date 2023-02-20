c0c88b433eed23a877ea33c75249a75ecb150c27
Marius Ortega <ortegasaurelmarius@gmail.com>
2023-02-20 18:01:39 +0100

    json export working

--- server/sandbox.js
+++ server/sandbox.js
@@ -2,8 +2,9 @@
 const dedicatedbrand = require('./eshops/dedicatedbrand');
 const montlimartbrand = require('./eshops/montlimartbrand');
 const circlesportswearbrand = require('./eshops/circlesportswearbrand');
+const fs = require('fs');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news', brandName = "dedicated") {
+async function sandbox (eshop = 'None', brandName = "all") {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
     var products;
@@ -18,18 +19,44 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news', br
       case 'circle':
         products = await circlesportswearbrand.scrape(eshop);
         break;
+      case 'all':
+        const dedicatedUrl = "https://www.dedicatedbrand.com/en/men/news";
+        const montlimartUrl = "https://www.montlimart.com/99-vetements";
+        const circleUrl = "https://shop.circlesportswear.com/collections/collection-homme";
+        const [dedicated, montlimart, circle] = await Promise.all([sandbox(dedicatedUrl, 'dedicated'), sandbox(montlimartUrl, 'montlimart'), sandbox(circleUrl, 'circle')])
+        return [dedicated, montlimart, circle];
     }
-    
-
-    console.log(products);
-    console.log('done');
-    process.exit(0);
+    return products;
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
 }
 
-const [,, eshop, brandName] = process.argv;
+async function ShowResult(result, allBrands) {
+  if (allBrands == "false") {
+    console.log(await result);
+  }
+  else {
+    for (let i=0; i<result.length; i++) {
+      console.log(await result[i]);
+    }
+  }
+}
 
-sandbox(eshop, brandName);
+async function ToFile(result) {
+  const toExport = JSON.stringify(await result);
+  fs.writeFileSync('D:/COURS/A4/S8 - ESILV/Web Architecture Applications/TP1/clear-fashion/server/ScrappingResult.txt', toExport);
+}
+
+const [,, allShops, eshop, brandName] = process.argv;
+
+if(allShops === "false") {
+  const res = sandbox(eshop, brandName);
+  ShowResult(res, allShops);
+  ToFile(res);
+}
+else {
+  const all = sandbox(eshop, brandName);
+  ShowResult(all, allShops);
+}
