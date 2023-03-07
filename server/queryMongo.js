274079aff226081554d77bcb28839f7ef990caa4
Marius Ortega <ortegasaurelmarius@gmail.com>
2023-03-07 16:20:01 +0100

    optimized query script

--- server/queryMongo.js
+++ server/queryMongo.js
@@ -6,7 +6,7 @@ function getClient() {
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     return client;
 }
-
+/*
 function FindAllBrandProduct(brandName = "dedicated") {
     const client = getClient();
     client.connect(async () => {
@@ -20,4 +20,43 @@ function FindAllBrandProduct(brandName = "dedicated") {
 });
 }
 
-FindAllBrandProduct("montlimart");
+function FindAllItemsLowerThanPrice(price){
+    const client = getClient();
+    client.connect(async () => {
+        const collection = client.db("clear-fashion").collection("products");
+        console.log()
+        const result = await collection.find({price:{$lt:price}}).toArray();
+        console.log(result);
+        console.log("#Items: ", result.length);
+    client.close();
+    return result;
+});
+}
+*/
+
+function FindAllBrandProduct(brandName = "dedicated") {
+    const script = {brand: `${brandName}`};
+    Query(script);
+}
+
+function FindAllItemsLowerThanPrice(price = 50) {
+    const script = {price:{$lt:price}};
+    Query(script);
+}
+
+
+function Query(script){
+    const client = getClient();
+    client.connect(async () => {
+        const collection = client.db("clear-fashion").collection("products");
+        console.log()
+        const result = await collection.find(script).toArray();
+        console.log(result);
+        console.log("#Items: ", result.length);
+    client.close();
+    return result;
+});
+}
+
+//FindAllBrandProduct("montlimart");
+FindAllItemsLowerThanPrice(50);
